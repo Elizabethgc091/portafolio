9db1126ebb7e65e2fd22f10fdf08446f436380a0
Elizabeth Gonzalez <elizabeth.gc091@gmail.com>
2022-05-26 20:29:13 -0500

    implementation header for desktop and mobile UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header className="header-container">
+        <section className="nav_container ">
+            <span className="text_name main-margin">Elizabeth González</span>
+            <nav className="nav margin-left">
+                {/*menu desktop*/}
+                <ul className="nav_list">
+                    <li className="nav_item">
+                        <span className='nav_link'>Resume</span>
+                    </li>
+                    <li className="nav_item">
+                        <span className='nav_link'>Projects</span>
+                    </li>
+                    <li className="nav_item">
+                        <span className='nav_link'>Contact</span>
+                    </li>
+                </ul>
+                {/*menu mobile*/}
+                <div className="mobile-navigation">
+                    <input type="checkbox" id="hamburger" />
+                    <label for="hamburger" className="hamburger-menu"><span className="top"></span>
+                        <span className="middle"></span>
+                        <span className="bottom"></span></label>
+                    <div className="menu-mobile list-mobile">
+                        <ul className="menu">
+                            <li>Resume</li>
+                            <li>Projects</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </section>
+     </header>
     </div>
   );
 }
